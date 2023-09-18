0ecfd171abb122f97ffea5e10e1b5dcfba0dcf51
farrukh0913 <farrukh.khan0913@gmail.com>
2023-09-18 19:36:43 +0500

    code commented

--- src/App.js
+++ src/App.js
@@ -10,33 +10,30 @@ import { LoginPage } from "./login/login-page";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button className="btn-create">Create <FcApproval style={{color: '#FFD701'}}/> </button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-
-      <span> <h2 className="title"> Hello World! </h2> </span>
-      </header>
-
-      <div className="car-section">
-        <Icon /> {Car()}
-      </div>
-
-      <div>
-        <LoginPage />
-      </div>
-
+      {/* <div aria-label="Login Section"> <Header /> </div> */}
+      {/* <div className="car-section" aria-label="Car Section"> <Icon /> {Car()} </div> */}
+      <div aria-label="Login Section"> <LoginPage /> </div>
     </div>
   );
+}
+
+function Header() {
+  return (<header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <button className="btn-create">Create <FcApproval style={{ color: '#FFD701' }} /> </button>
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+  </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer">
+      Learn React
+  </a>
+
+    <span> <h2 className="title"> Hello World! </h2> </span>
+  </header>)
 }
 
 export default App;
--- src/login/login-page.js
+++ src/login/login-page.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
 import "./login-page.css";
 
 export function LoginPage() {
