d7602f01b0f6e7210a1cd0b30862c0214faa7477
farrukh0913 <farrukh.khan0913@gmail.com>
2023-09-28 18:40:03 +0500

    all fields bindings

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,36 +1,79 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { MDBRow, MDBRadio, MDBSpinner, MDBCol, MDBFile, MDBInput, MDBCheckbox, MDBBtn, MDBSwitch, MDBInputGroup, MDBInputGroupElement, MDBInputGroupText } from 'mdb-react-ui-kit';
 import Form from 'react-bootstrap/Form';
 import './home-page.scss';
 
 const HomePage = () => {
+  // const state = { firstName: '', lastName: '', email: '', contact: '' };
+  const [form, setForm] = React.useState({
+    firstName: "",
+    lastName: "",
+    gender: { male: false, female: false },
+    stack: { angular: false, react: false },
+    nationality: false,
+    country: "",
+    cnic: "",
+    email: "",
+    phone: "",
+    married: false,
+    file: ""
+  });
+
+  const onFieldValueChange = (event) =>{
+    console.log('event:123 ', event);
+    const name: string = event.target.name;
+    console.log('name: ', name);
+    const id: string = event.target.id;
+    console.log('id: ', id);
+    const value: string = event.target.value;
+    console.log('value: ', value);
+    const checkedStatus: boolean = event.target.checked;
+    if(id === 'nationality' || id === 'married'){
+      console.log('checkedStatus: ', checkedStatus);
+      setForm({...form, [name]: checkedStatus});
+    } else if(id === 'radio') {
+      const gender = { male: (value === 'male'), female: (value === 'female') }
+      setForm({...form, gender: gender});
+    }else if(id === 'checkbox') {
+      const stack = { angular: (name === 'angular' || form.stack.angular), react: (name === 'react' || form.stack.react) }
+      setForm({...form, stack: stack});
+    }else{
+      setForm({...form, [name]: value});
+    }
+
+  }
+
+  const onFormSubmit = (e)=>{
+    console.log('this.form :>> ', form);
+  }
+
   return <div>
     <span>Home Page Working!</span>
     <form className="form">
       <MDBRow className='mb-4'>
         <MDBCol>
-          <MDBInput id='form6Example1' label='First name' />
+          <MDBInput id='input1' name="firstName" label='First Name' value={form.firstName} onChange={onFieldValueChange} />
         </MDBCol>
         <MDBCol>
-          <MDBInput id='form6Example2' label='Last name' />
+          <MDBInput id='input2' name="lastName" label='Last Name' value={form.lastName} onChange={onFieldValueChange} />
         </MDBCol>
       </MDBRow>
 
       <div className='d-flex justify-content-between align-items-center'>
         <div className="radio-btns">
-          <MDBRadio name='inlineRadio' id='inlineRadio1' value='option1' label='Male' inline />
-          <MDBRadio name='inlineRadio' id='inlineRadio2' value='option2' label='Female' inline />
+          <MDBRadio name='gender' id='radio' value='male' label='Male' checked={form.gender.male} onChange={onFieldValueChange} inline />
+          <MDBRadio name='gender' id='radio' value='female' label='Female' checked={form.gender.female} onChange={onFieldValueChange} inline />
         </div>
-        <MDBSwitch id='flexSwitchCheckDefault' label='Default switch checkbox input' />
+        <MDBSwitch id='nationality' name="nationality" label='Nationality' value={form.nationality} onChange={onFieldValueChange} />
 
         <div>
-          <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Default checkbox' />
-          <MDBCheckbox name='flexCheck' value='' id='flexCheckChecked' label='Checked checkbox' defaultChecked />
+          <MDBCheckbox name='angular' id='checkbox' label='Angular' value={form.stack.angular} onChange={onFieldValueChange} />
+          <MDBCheckbox name='react' id='checkbox' label='React' value={form.stack.react} onChange={onFieldValueChange} />
         </div>
       </div>
 
       <div className='mb-4'>
-        <Form.Select aria-label="Default select example">
+        <Form.Select name="country" value={form.country} onChange={onFieldValueChange}>
           <option>Country</option>
           <option value="1">Pakistan</option>
           <option value="2">UAE</option>
@@ -38,20 +81,19 @@ const HomePage = () => {
         </Form.Select>
       </div>
 
-      <MDBInput wrapperClass='mb-4' label='Company name' />
-      <MDBInput wrapperClass='mb-4' label='CNIC' />
-      <MDBInput wrapperClass='mb-4' type='email' label='Email' />
-      <MDBInput wrapperClass='mb-4' type='tel' label='Phone' />
-      <MDBInput wrapperClass='mb-4' className='additional-info-field' label='Additional information' />
-      <MDBCheckbox wrapperClass='d-flex justify-content-start mb-4' label='Create an account?' defaultChecked />
+      <MDBInput wrapperClass='mb-4' id="cnic" name="cnic" label='CNIC' value={form.cnic} onChange={onFieldValueChange} />
+      <MDBInput wrapperClass='mb-4' type='email' name="email" label='Email' value={form.email} onChange={onFieldValueChange} />
+      <MDBInput wrapperClass='mb-4' type='tel' name="phone" label='Phone' value={form.phone} onChange={onFieldValueChange} />
+      <MDBCheckbox wrapperClass='d-flex justify-content-start mb-4' id="married" name="married" label='Married?' value={form.married} onChange={onFieldValueChange} />
+
       <div className='mb-4'>
-        <MDBFile id='customFile' />
+        <MDBFile id='file' name="file" value={form.file} onChange={onFieldValueChange} />
       </div>
 
       {/* Spinner */}
       {/* <MDBSpinner role='status'> <span className='visually-hidden'>Loading...</span> </MDBSpinner>  */}
 
-      <MDBBtn className='mb-4 btn-submit' type='submit'> Save </MDBBtn>
+      <MDBBtn className='mb-4 btn-submit' type='button' onClick={onFormSubmit}> Save </MDBBtn>
     </form>
   </div>;
 };
